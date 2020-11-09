0f286ba43e377ccbf436164c9ea0219776b2729d
VinceDeslo <v.desloover2@gmail.com>
2020-11-09 12:57:42 -0500

    Successful queries to MySQL container from go code.

---
+++ MySQL/connection.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("Entering MySQL connection program.")
+
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/db")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Opened db successfully.")
+	defer db.Close()
+
+	// Create a table for the entries
+	_, err = db.Exec("CREATE Table members(id int NOT NULL AUTO_INCREMENT, first_name varchar(50), last_name varchar(50), PRIMARY KEY(id));")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Created members table successfully.")
+
+	// Insert a new entry into the mysql database
+	insert, err := db.Query("INSERT INTO members (first_name, last_name) VALUES ('Vincent','Desloover')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Inserted a member entry successfully.")
+
+	defer insert.Close()
+}
