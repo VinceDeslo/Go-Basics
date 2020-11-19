da7f48fb4212e3d62e9c364c33a83db18c7a6430
VinceDeslo <v.desloover2@gmail.com>
2020-11-19 10:21:25 -0500

    MongoDB Document entry successful with auth

--- MongoDB/connection.go
+++ MongoDB/connection.go
@@ -16,26 +16,51 @@ type Employee struct {
 	Salary     int
 }
 
+// Error validation call
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
 	fmt.Println("Starting MongoDB connection program.")
 
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://root:rootpassword@localhost:27017")
 
 	// Connect to Mongo
 	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 
 	// Test the connection
 	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 
 	fmt.Println("Connection to Mongo successful.")
+
+	// Fetch handle for collection
+	collection := client.Database("test").Collection("employees")
+
+	// Data instanciation
+	employee1 := Employee{"Steve", "IT", 75000}
+	employee2 := Employee{"Marie", "HR", 50000}
+	employee3 := Employee{"John", "Accounting", 56000}
+
+	// Append a single employee document to collection
+	insertResult, err := collection.InsertOne(context.TODO(), employee1)
+	checkErr(err)
+	fmt.Println("Inserted an employee entry: ", insertResult.InsertedID)
+
+	// Append multiple employee documents to collection
+	employees := []interface{}{employee2, employee3}
+	insertManyResult, err := collection.InsertMany(context.TODO(), employees)
+	checkErr(err)
+	fmt.Println("Inserted many employee entries:", insertManyResult.InsertedIDs)
+
+	// Disconnect the client
+	err = client.Disconnect(context.TODO())
+	checkErr(err)
+	fmt.Println("Connection to Mongo closed.")
 }
